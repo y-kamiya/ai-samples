94f033969a8e4d88181d24aed305836af659e265
Yuji Kamiya <y.kamiya0@gmail.com>
2016-09-25 14:23:38 +0900

    modify definition of Term

--- app/BlockWorld.hs
+++ app/BlockWorld.hs
@@ -5,11 +5,11 @@ import qualified Data.Map as M
 data Block = A | B  deriving (Show, Eq, Ord, Enum)
 data Object = Table | Object Block deriving Show
 
-data HandState = HandEmpty | HandHas Block deriving Show
-data IsTop = IsTop Block Bool deriving Show
-data On = On Block Object deriving Show
-
-data Term = HandState | IsTop | On deriving Show
+data Term = HandEmpty
+          | HandHas Block
+          | IsTop Block Bool
+          | On Block Object
+          deriving Show
 
 type Condition = [Term]
 
@@ -30,21 +30,21 @@ type Plan = [ActionType]
 type Domain = [Action]
 
 data NodeInfo = NodeInfo { condition :: Condition
-                         , action :: Action
-                         , next :: NodeInfo
-                         , realCost :: Int
-                         , score :: Int
-                         , diff :: Condition
+                         , action    :: Action
+                         , next      :: NodeInfo
+                         , realCost  :: Int
+                         , score     :: Int
+                         , diff      :: Condition
                          , diffCount :: Int
                          } deriving Show
 
 main :: IO ()
 main = do
-  let startCondition = [HandEmpty, IsTop A True, IsTop B False, On A Object B, On B Table]
-  let goalCondition  = [HandEmpty IsTop A False, IsTop B True, On B Object A, On A Table]
+  let startCondition = [HandEmpty, IsTop A True, IsTop B False, On A (Object B), On B Table]
+  let goalCondition  = [HandEmpty, IsTop A False, IsTop B True, On B (Object A), On A Table]
   mapM_ print buildDomain
-  let plan = strips buildDomain startCondition goalCondition    
-  return ()
+  -- let plan = strips buildDomain startCondition goalCondition    
+  -- return ()
 
 
 buildDomain :: Domain
@@ -64,28 +64,28 @@ buildAction cost aType@(Putdown x) = Action aType (buildPre x) (buildPost x) cos
   where buildPre x  = [HandHas x, IsTop x False]
         buildPost x = [HandEmpty, IsTop x True, On x Table]
 buildAction cost aType@(Stack x y) = Action aType (buildPre x y) (buildPost x y) cost
-  where buildPre x y  = [HandHas x, IsTop x False, IsTop y,True]
-        buildPost x y = [HandEmpty, IsTop x True, IsTop y,False, On x (Object y)]
+  where buildPre x y  = [HandHas x, IsTop x False, IsTop y True]
+        buildPost x y = [HandEmpty, IsTop x True, IsTop y False, On x (Object y)]
 buildAction cost aType@(Unstack x y) = Action aType (buildPre x y) (buildPost x y) cost
   where buildPre x y  = [HandEmpty, IsTop x True, IsTop y False, On x (Object y)]
         buildPost x y = [HandHas x, IsTop x False, IsTop y True]
 
 strips :: Domain -> Condition -> Condition -> Plan
-strips domain start goal = extractPlan [] . searchPlan
+strips domain start goal = extractPlan [] $ searchPlan domain start goal
   where
     extractPlan :: Plan -> NodeInfo -> Plan
     extractPlan plan nodeInfo
       | realCost nodeInfo == 0 = newPlan
       | otherwise = extractPlan newPlan $ next nodeInfo
       where
-        newPlan = (actionType nodeInfo) : plan
+        newPlan = (actionType $ action nodeInfo) : plan
 
 
 searchPlan :: Domain -> Condition -> Condition -> NodeInfo
-searchPlan = domain start goal = searchNext domain goal [buildNodeInfo goal] [] 
+searchPlan domain start goal = searchNext domain goal [] [] 
   where
-    buildGoalNodeIfo = NodeInfo goal NoAction NoNodeInfo 0 score diff diffCount
+    -- buildGoalNodeIfo = NodeInfo goal NoAction NoNodeInfo 0 score diff diffCount
 
     searchNext :: Domain -> Condition -> [NodeInfo] -> [NodeInfo] -> NodeInfo
-    searchNext [] _ _ = NoNodeInfo
+    -- searchNext [] _ _ = NoNodeInfo
     searchNext domain (nodeInfo:rest) closeList = undefined
