e1a37aba6fc490d5316b35e38db3b00b853d82e0
Yuji Kamiya <y.kamiya0@gmail.com>
2016-10-20 09:58:32 +0900

    breakdown

--- src/Htn.hs
+++ src/Htn.hs
@@ -1,9 +1,9 @@
 module Htn where
 
-data PrimitiveTask = Stack Block Block Condition Condition
-                   | UnStack Block Block Condition Condition
-                   | Putdown Block Condition Condition
-                   | Pickup Block Condition Condition
+data PrimitiveTask = Stack Block Block
+                   | UnStack Block Block
+                   | Putdown Block
+                   | Pickup Block
 
 data CompoundTask = Move Block Object
                   | Clear Block
@@ -13,33 +13,41 @@ data CompoundTask = Move Block Object
 data Task = Primitive PrimitiveTask
           | Compound CompoundTask
 
-type Domain = [Task]
+newtype Domain = Domain {
+                 primitiveMap :: M.Map PrimitiveTask [(Condition, Condition)]
+               , compoundMap :: M.Map CompoundTask [(Condition, [Task])]
+               }
 
 htn :: Domain -> Condition -> [Task] -> [Task]
-htn domain condition tasks = tasks
+htn domain condition tasks = htn' domain condition tasks []
 
 htn' :: Domain -> Condition -> [Task] -> [Task] -> [Task]
 htn' _ _ [] plan = plan
 htn' domain [] _ plan = plan ++ Invalid
 htn' domain condition (task@(Invalid):tasks) plan = plan ++ task
-htn' domain condition (task@(Primitive):tasks) plan = let newCondition = execute condition task
+htn' domain condition (task@(Primitive):tasks) plan = let newCondition = execute domain condition task
                                                       in  htn' domain newCondition tasks $ plan ++ task
-htn' domain condition (task@(Compound):tasks) plan = let newTasks = breakdown condition task
+htn' domain condition (task@(Compound):tasks) plan = let newTasks = breakdown domain condition task
                                                      in  htn' domain condition (newTasks ++ tasks) plan
   
 
-breakdown :: Condition -> CompoundTask -> [Task]
-breakdown condition (Move a b)
+breakdown :: Doamin -> Condition -> CompoundTask -> [Task]
+breakdown domain condition task@(Move a b) = case M.lookup task (compoundMap domain) of
+                                               Just list -> case find (\(pre, _) -> include condition pre) list of
+                                                              Just (_, tasks) -> tasks
+                                                              Nothing -> [Invalid]
+                                               Nothing -> [Invalid]
+-- Conditionが存在しない場合が必要＝otherwise
+-- Matchするconditionを探してそのpost conditionを返す
+
   | satisfy condition [Hand a, IsTop b True]  = [Put a b]
   | satisfy condition [Hand a]                = [Clear b, Put a b]
   | satisfy condition [IsTop a False]         = [Clear a, Move a b]
   | satisfy condition [IsTop b False] && b /= Table = [Clear b, Move a b]
   | otherwise                                 = [Get a, Put a b]
 
-execute :: Condition -> PrimitiveTask -> Condition
-execute condition (Stack x y pre post) = if satisfy pre condition
-                                           then post
-                                           else []
+execute :: Doamin -> Condition -> PrimitiveTask -> Condition
+execute domain condition (Stack x y pre post) = if satisfy pre condition then post else []
 
 
 
